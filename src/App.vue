395908e08d6756f0ef681dc4baceafc1d9d7c2b4
Govind S <govindsd@peopleintouch.com>
2021-04-19 23:54:49 +0200

    configured main app file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  component(is = "DefaultLayout")
+    Home
+</template>
+
+<script>
+import Home from "./views/home"
+import DefaultLayout from "@/layouts/default"
+
+export default {
+  name: "WebWorkerSorting",
+  components: {
+    Home,
+    DefaultLayout
+  }
+}
+
+</script>
